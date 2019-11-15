bcb76d1e7db36eeab7efcc950cc866c30f309657
Amendoza20 <20.angel.mendoza@gmail.com>
2019-11-14 20:43:08 -0500

    music test

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,7 +8,42 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selectionRight(Integer startIndex, String selection){
+        int index = startIndex;
+        for(int i = 0; i <playList.length / 2; i++){
+           if(playList[index].equals(selection)){
+               return i;
+           }
+             index++;
+           if(index == playList.length) {
+               index = 0;
+           }
+
+
+        }
+
+        return playList.length/2;
+    }
+    public Integer selectionLeft (Integer startIndex, String selection){
+        int index = startIndex;
+        for(int i = 0 ; i <playList.length / 2; i++){
+            if (playList[index].equals(selection)){
+                return i;
+            }
+                index--;
+            if(index == -1){
+                index = playList.length -1;
+            }
+        }
+        return playList.length / 2;
+    }
+
+    public Integer selection (Integer startIndex, String selection){
+        Integer right = selectionRight(startIndex, selection);
+        Integer left = selectionLeft(startIndex, selection);
+        if(right < left){
+            return right;
+        }
+        return left;
     }
 }
